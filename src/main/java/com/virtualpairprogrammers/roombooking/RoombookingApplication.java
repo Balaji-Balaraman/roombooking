c6d825722d58b31e9884b089ddb3f2e30cde62d4
Balaji-Balaraman <balaji.balaraman@zucisystems.com>
2024-03-22 10:09:27 +0000

    1 try

--- src/main/java/com/virtualpairprogrammers/roombooking/RoombookingApplication.java
+++ src/main/java/com/virtualpairprogrammers/roombooking/RoombookingApplication.java
@@ -1,9 +1,12 @@
 package com.virtualpairprogrammers.roombooking;
 
+import org.apache.axis.AxisProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class RoombookingApplication extends SpringBootServletInitializer {
@@ -15,5 +18,13 @@ public class RoombookingApplication extends SpringBootServletInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 		return builder.sources(RoombookingApplication.class);
+	}
+
+	@PostConstruct
+	void preventAxisPropertiesClassLoaderLeak() {
+		ClassLoader tccl = Thread.currentThread().getContextClassLoader();
+		Thread.currentThread().setContextClassLoader(AxisProperties.class.getClassLoader());
+		AxisProperties.getNameDiscoverer();
+		Thread.currentThread().setContextClassLoader(tccl);
 	}
 }
